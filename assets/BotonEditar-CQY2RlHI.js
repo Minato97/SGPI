3412ac29e03fe2cb8876022554dc2da9afe81673
Minato97 <ignacio.andrade8033@alumnos.udg.mx>
2026-09-18 12:09:34 -0600

    Subir archivos de dist/spa

---
+++ assets/BotonEditar-CQY2RlHI.js
@@ -0,0 +1,1 @@
+import{Q as e}from"./QBtn-BoIK7OKJ.js";import{o as r,a}from"./index-CISa6VFx.js";const p={__name:"BotonEditar",props:{label:{type:String,default:"Editar"}},setup(o){const t=o;return(l,n)=>(r(),a(e,{rounded:"",label:t.label,type:"submit",color:"secondary",icon:"edit"},null,8,["label"]))}};export{p as _};
---
+++ assets/SGPI_LOGO-xPWlwdUp.js
@@ -0,0 +1,1 @@
+const s="/SGPI/assets/SGPI_LOGO-DTuPnYXU.png";export{s as _};
